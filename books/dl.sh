44367602403586dde5d275f9d5b374fc05ceee82
Myron Marintsev <marintsev@gmail.com>
2016-01-31 12:36:51 +0300

    Скрипт, скачивающий книги.
    Простейший модуль ядра и makefile для его сборки из книги.
    
    symlink на исходники ядра 2.6.32 для SL 6.7.

---
+++ books/dl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget -c 'http://examples.oreilly.com/9780596005900/examples.tar.gz'
+wget -c 'http://free-electrons.com/doc/books/ldd3.pdf'
+wget -c 'http://ilinuxkernel.com/files/Linux.Kernel.Development.3rd.Edition.pdf'
---
+++ do.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+make -C ./kernel-2.6.32 M=`pwd` modules
