d8363df2f8130796e00c0b823fdb4e03a8b4b9ba
Myron Marintsev <marintsev@gmail.com>
2016-02-16 19:50:55 +0300

    Создаю блочное устройство.

--- Makefile
+++ Makefile
@@ -1,1 +1,18 @@
-obj-m := main.o
+ifneq ($(KERNELRELEASE),)
+	obj-m := main.o
+
+else
+	KERNELDIR ?= /lib/modules/$(shell uname -r)/build
+	PWD := $(shell pwd)
+
+default:
+	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
+
+mkdev:
+	sudo mknod wikipedia b 137 0
+	sudo chmod 666 wikipedia
+
+rmdev:
+	sudo rm wikipedia
+
+endif
--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#include <linux/fs.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init( void )
 {
+    int code;
+
     printk( KERN_ALERT "Hello, world\n" );
+
+	// устройство будет видно в /proc/devices
+    code = register_blkdev( 137, "wikipedia" );
+    printk( KERN_ALERT "register_blkdev(137,\"wikipedia\") => %d", code );
+    if( code < 0 ) // ошибка
+    {
+    	// TODO:
+    }
+    
     return 0;
 }
 
